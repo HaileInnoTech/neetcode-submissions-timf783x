8f02bd19b8061ad2f0db69d56dbbccde5f07c2a4
Hai Le <hai28022002@gmail.com>
2026-04-04 21:25:41 +0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        int cur = 0;
+        int  next = cur+1;
+        while(next < prices.length){
+            if(prices[cur] < prices[next]){
+                int profit = prices[next] - prices[cur];
+                res = Math.max(profit, res);
+                next = next + 1;
+            }
+            else{
+                cur = next;
+                next= cur+1;
+            }
+        }
+        return res;
+        
+    }
+}
